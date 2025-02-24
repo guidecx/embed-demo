18fb997a953c0418128b6802b8a9684175a4840a
Alex Nelson <w.alexnelson@gmail.com>
2025-02-24 10:00:19 -0700

    use .env (#3)

--- src/pages/api/embed-token.js
+++ src/pages/api/embed-token.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const gcxOpenApiToken = ''
-  const gcxApiUrl = ''
-  const customerEmail = ''
+  const gcxOpenApiToken = process.env.API_TOKEN
+  const gcxApiUrl = process.env.EMBED_API
+  const customerEmail = process.env.NEXT_PUBLIC_CUSTOMER_EMAIL
 
   try {
     const request = await fetch(gcxApiUrl, {
